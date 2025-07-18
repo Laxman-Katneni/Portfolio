e07304dd9b8439c9ec31402e2a9ff8a5088c5e2e
Laxman Katneni <143445097+Laxman-Katneni@users.noreply.github.com>
2025-07-18 18:51:42 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -76,7 +76,7 @@ function Header({ isDarkMode }: Props) {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="Resume2025.pdf"
+          href="LaxmanResume2025.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 light-hover duration-500 bg-white dark:text-black"
         >
